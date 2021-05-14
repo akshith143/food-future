c1163c4299d36f2093d83adb77542580c954c89a
akshith.p <akshith.p@cargoexchange.in>
2021-05-15 00:27:34 +0530

    new changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 window.onscroll = function () {
     console.log("scrolled")
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.height = "50px";
-        document.getElementById("nav-logo").style.height = "50px";
-        document.getElementById("nav-logo").style.width = "50px";
+        document.getElementById("header").style.height = "80px";
+        document.getElementById("nav-logo").style.height = "80px";
+        document.getElementById("nav-logo").style.width = "80px";
     } else {
-        document.getElementById("header").style.height = "100px";
-        document.getElementById("nav-logo").style.height = "100px";
-        document.getElementById("nav-logo").style.width = "100px";
+        document.getElementById("header").style.height = "150px";
+        document.getElementById("nav-logo").style.height = "150px";
+        document.getElementById("nav-logo").style.width = "150px";
     }
 }
