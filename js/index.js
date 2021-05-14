c322e1a0fb9c00dae24777a1dc24a312a83ce852
akshith.p <akshith.p@cargoexchange.in>
2021-05-15 01:13:32 +0530

    landing page changes

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 window.onscroll = function () {
     console.log("scrolled")
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("header").style.height = "80px";
-        document.getElementById("nav-logo").style.height = "80px";
-        document.getElementById("nav-logo").style.width = "80px";
+        document.getElementById("header").style.height = "45px";
+        document.getElementById("nav-logo").style.height = "45px";
+        document.getElementById("nav-logo").style.width = "45px";
     } else {
-        document.getElementById("header").style.height = "150px";
-        document.getElementById("nav-logo").style.height = "150px";
-        document.getElementById("nav-logo").style.width = "150px";
+        document.getElementById("header").style.height = "93px";
+        document.getElementById("nav-logo").style.height = "93px";
+        document.getElementById("nav-logo").style.width = "93px";
     }
 }
